75e92a53e4c4df206518df667947c0b6f56d83c7
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-12 13:07:14 +0500

    Created Apps and their views

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from . import views
+from shop import urls as shop_urls
+from blog import urls as blog_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="home"),
+    path('shop/', include(shop_urls, namespace="shop")),
+    path('blog/', include(blog_urls, namespace="blog")),
 ]
