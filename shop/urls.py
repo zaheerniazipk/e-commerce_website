df4147125ab5a6715d7219c0c5d3b06685cdf4bd
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-12 15:35:57 +0500

    Project Layout and url mapping

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,10 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.index, name='shop-home'),
+    path('about/', views.about, name="about-us"),
+    path('contact/', views.contact, name="contact-us"),
+    path('tracker/', views.tracker, name="tracking-status"),
+    path('search/', views.search, name="search"),
+    path('productview/', views.productView, name="product-view"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,27 @@ from django.http import HttpResponse
 def index(request):
     # return HttpResponse("Index of Shop")
     return render(request, "shop/index.html")
+
+
+def about(request):
+    return HttpResponse("We are at about page")
+
+
+def contact(request):
+    return HttpResponse("We are at contact page")
+
+
+def tracker(request):
+    return HttpResponse("We are at tracker page")
+
+
+def search(request):
+    return HttpResponse("We are at search page")
+
+
+def productView(request):
+    return HttpResponse("We are at productView page")
+
+
+def checkout(request):
+    return HttpResponse("We are at checkout page")
